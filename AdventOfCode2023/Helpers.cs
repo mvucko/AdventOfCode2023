3cc1cc8bcdd580c5fb5812ae64e4a548887a7d74
mvucko <mislav.vucko@gmail.com>
2023-12-07 21:09:43 +0100

    Task 3. Part 1. Helper for between number extension method

--- AdventOfCode2023/Helpers.cs
+++ AdventOfCode2023/Helpers.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AdventOfCode2023
 {
-    internal class Helpers
+    internal static class Helpers
     {
         internal static string[] ReadAllFromInputFile(string file)
         {
             string basedir = "input";
             return File.ReadAllLines($"{basedir}/{file}.txt");
+        }
+
+        public static bool Between<T>(this T value, T min, T max) where T : INumber<T>
+        {
+            return value >= min && value <= max;
         }
     }
 }
---
+++ AdventOfCode2023/Solutions/Day03.cs
@@ -0,0 +1,113 @@
+﻿namespace AdventOfCode2023.Solutions
+{
+    internal class Day03 : ISolution
+    {
+        public int TaskNumber => 3;
+
+        public void Solve()
+        {
+            try
+            {
+                var data = Helpers.ReadAllFromInputFile("task3");
+                var symbols = GetSymbols(data);
+                var numbers = GetNumbers(data);
+                List<int> result = CheckAdjacency(symbols, numbers);
+                Console.WriteLine(result.Sum());
+            }
+            catch (Exception ex) { }
+
+
+
+        }
+
+        private List<int> CheckAdjacency(List<Vector2Int> symbols, List<Vector2IntWithLengthAndNumber> numbers)
+        {
+            var result = new List<int>();
+            numbers = numbers.OrderBy(x => x.Y).ToList();
+            foreach (var item in numbers)
+            {
+                var adjacent = symbols.Where(sym => sym.X.Between(item.X - 1, item.X + item.Length) && sym.Y.Between(item.Y - 1, item.Y + 1));
+                if (adjacent.Any())
+                {
+                    result.Add(item.Number);
+                }
+            }
+            return result;
+
+        }
+
+        private List<Vector2Int> GetSymbols(string[] data)
+        {
+            List<Vector2Int> symbols = new();
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    if (data[i][j] != '.' && !char.IsNumber(data[i][j]))
+                    {
+                        symbols.Add(new() { X = j, Y = i });
+                    }
+                }
+            }
+            return symbols;
+        }
+
+        private List<Vector2IntWithLengthAndNumber> GetNumbers(string[] data)
+        {
+            List<Vector2IntWithLengthAndNumber> numbers = new List<Vector2IntWithLengthAndNumber>();
+            for (int i = 0; i < data.Length; i++)
+            {
+                for (int j = 0; j < data[i].Length; j++)
+                {
+                    if (char.IsNumber(data[i][j]))
+                    {
+                        Vector2IntWithLengthAndNumber toAdd = new Vector2IntWithLengthAndNumber()
+                        {
+                            X = j,
+                            Y = i
+                        };
+                        string number = data[i][j].ToString();
+                        bool hasMore = true;
+                        int length = 1;
+                        while (hasMore)
+                        {
+                            if (j + 1 < data[i].Length)
+                            {
+                                if (char.IsNumber(data[i][j + 1]))
+                                {
+                                    length++;
+                                    number += data[i][j + 1].ToString();
+                                    j++;
+                                }
+                                else
+                                {
+                                    hasMore = false;
+                                }
+                            }
+                            else
+                            {
+                                hasMore = false;
+                            }
+                        }
+                        toAdd.Length = length;
+                        toAdd.Number = int.Parse(number);
+                        numbers.Add(toAdd);
+                    }
+                }
+            }
+            return numbers;
+        }
+
+        private class Vector2Int()
+        {
+            public int X;
+            public int Y;
+        }
+
+        private class Vector2IntWithLengthAndNumber() : Vector2Int
+        {
+            public int Length;
+            public int Number;
+        }
+    }
+}
